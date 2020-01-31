6696189217f84370a53173941a92f5ac36293f36
plasticuproject <19690649+plasticuproject@users.noreply.github.com>
2020-01-30 22:14:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Flask Tutorial for a prototype API for distant reading of science fiction novels.
+> **Source:**
+> https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask
---
+++ api_json.py
@@ -0,0 +1,54 @@
+import json
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = False
+
+
+# Load test data from JSON file for our catalog in the form of a list of dictionaries.
+with open('books.json', 'r') as datafile:
+    books = json.loads(datafile.read())
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+    <p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify(books)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+    for book in books:
+        if book['id'] == id:
+            results.append(book)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
+
+app.run()
